c35c7bcc24a65d92c6055f7966c8f93f4af7a901
Shubham Raichandel <shubhamraichandel@yahoo.com>
2019-04-19 23:36:11 -0400

    changes in ejs

--- server/config/db.js
+++ server/config/db.js
@@ -1,5 +1,5 @@
 module.exports = {
-   //"URI" : "mongodb://localhost/test",
-    "URI" : "mongodb://shubham:a123456@ds036967.mlab.com:36967/xresearch",
-    "secret": "SomeSecret"
-}
+  //local MongoDB deployment ->
+  "URI" : "mongodb://shubham:a123456@ds036967.mlab.com:36967/xresearch"
+  
+};
