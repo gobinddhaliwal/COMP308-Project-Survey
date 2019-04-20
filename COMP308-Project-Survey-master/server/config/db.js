49ac30dc55ac826a889d555046ba1c4fb029092e
gobinddhaliwal <dhaliwalgobind@gmail.com>
2019-04-20 03:56:28 -0400

    Change made in survey

---
+++ COMP308-Project-Survey-master/server/config/db.js
@@ -0,0 +1,6 @@
+module.exports = {
+  //local MongoDB deployment ->
+ "URI" : "mongodb://localhost/survey"
+ // "URI" : "mongodb://shubham:a123456@ds036967.mlab.com:36967/xresearch"
+  
+};
---
+++ COMP308-Project-Survey-master/server/controllers/index.js
@@ -0,0 +1,18 @@
+
+// Display the Home Page
+module.exports.DisplayHome = (req, res) => {
+  res.render('content/index', {
+    title: 'Home',
+    games: '',
+    displayName: req.user ? req.user.displayName : ''
+   });
+}
+
+// Displays the Contact Page
+module.exports.DisplayContact = (req, res) => {
+  res.render('content/contact', {
+    title: 'Contact',
+    games: '',
+    displayName: req.user ? req.user.displayName : ''
+   });
+}
